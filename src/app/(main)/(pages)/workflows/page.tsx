8b683648920bd108cc652d031026f902919f11b8
zifrz <114908186+zifrz@users.noreply.github.com>
2024-12-04 15:06:26 +0530

    add: workflows page

--- src/app/(main)/(pages)/workflows/page.tsx
+++ src/app/(main)/(pages)/workflows/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import WorkflowButton from "./_components/workflow-button";
 
 type Props = {};
 
@@ -7,6 +8,7 @@ const Workflows = (props: Props) => {
     <div className="flex flex-col gap-4 relative">
       <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b justify-between">
         Workflows
+        <WorkflowButton />
       </h1>
     </div>
   );
