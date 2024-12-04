181068925e411a9c1ac8cd6961888599e3917400
zifrz <114908186+zifrz@users.noreply.github.com>
2024-12-04 15:06:26 +0530

    add: workflows page

---
+++ src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useModal } from "@/providers/modal-provider";
+import { Plus } from "lucide-react";
+import React from "react";
+
+type Props = {};
+
+const WorkflowButton = (props: Props) => {
+  const { setOpen, setClose } = useModal();
+  const handleClick = () => {};
+
+  return (
+    <Button size={"icon"} onClick={handleClick}>
+      <Plus />
+    </Button>
+  );
+};
+
+export default WorkflowButton;
