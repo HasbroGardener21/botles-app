a996edf2b2b91fbe44da6dd33979c44d73c248dc
zifrz <114908186+zifrz@users.noreply.github.com>
2024-12-04 13:56:17 +0530

    add: connect to connections button

--- src/app/(main)/(pages)/connections/_components/connection-card.tsx
+++ src/app/(main)/(pages)/connections/_components/connection-card.tsx
@@ -7,6 +7,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   type: ConnectionTypes;
@@ -14,7 +15,7 @@ type Props = {
   title: ConnectionTypes;
   description: string;
   callback?: () => void;
-  connected: {} & any;
+  // connected: {} & any;
 };
 
 const ConnectionCard = ({
@@ -22,7 +23,7 @@ const ConnectionCard = ({
   type,
   icon,
   title,
-  connected,
+  // connected,
 }: Props) => {
   return (
     <Card className="flex w-full items-center justify-between">
@@ -41,6 +42,27 @@ const ConnectionCard = ({
           <CardDescription>{description}</CardDescription>
         </div>
       </CardHeader>
+      <div className="flex flex-col items-center gap-2 p-4">
+        {/* {connected[type] ? (
+          <div className="border-bg-primary rounded-lg border-2 px-3 py-2 font-bold text-white">
+            Connected
+          </div>
+        ) : ( */}
+          <Link
+            href={
+              title == "Discord"
+                ? process.env.NEXT_PUBLIC_DISCORD_REDIRECT!
+                : title == "Notion"
+                ? process.env.NEXT_PUBLIC_NOTION_AUTH_URL!
+                : title == "Slack"
+                ? process.env.NEXT_PUBLIC_SLACK_REDIRECT!
+                : "#"
+            }
+            className=" rounded-lg bg-primary p-2 font-bold text-primary-foreground">
+            Connect
+          </Link>
+        {/* )} */}
+      </div>
     </Card>
   );
 };
