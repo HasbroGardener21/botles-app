944e0291b83d7644a272383d7204b768b304f67a
zifrz <114908186+zifrz@users.noreply.github.com>
2024-12-04 16:07:49 +0530

    add: ModalProvider in RooLayout, Workflow-Add button basic implementation

--- src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Workflowform from "@/components/forms/workflow-form";
+import CustomModal from "@/components/global/custom-modal";
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/providers/modal-provider";
 import { Plus } from "lucide-react";
@@ -8,7 +10,15 @@ type Props = {};
 
 const WorkflowButton = (props: Props) => {
   const { setOpen, setClose } = useModal();
-  const handleClick = () => {};
+  const handleClick = () => {
+    setOpen(
+      <CustomModal
+        title="Create a Workflow Automation"
+        subheading="Workflows are a powerfull that help you automate tasks.">
+        <Workflowform />
+      </CustomModal>
+    );
+  };
 
   return (
     <Button size={"icon"} onClick={handleClick}>
